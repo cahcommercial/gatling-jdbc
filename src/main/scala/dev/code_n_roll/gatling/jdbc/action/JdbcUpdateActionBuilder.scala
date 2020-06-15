36efb54714e92f92bbf780a58d0050372f67b743
cah-bilalshahid <bilal.shahid@cardinalhealth.com>
2020-06-15 10:05:32 -0400

    add update action

--- src/main/scala/dev/code_n_roll/gatling/jdbc/action/JdbcUpdateActionBuilder.scala
+++ src/main/scala/dev/code_n_roll/gatling/jdbc/action/JdbcUpdateActionBuilder.scala
@@ -1,5 +1,10 @@
 package dev.code_n_roll.gatling.jdbc.action
 
+import io.gatling.core.action.Action
+import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.core.session.Expression
+import io.gatling.core.structure.ScenarioContext
+
 case class JdbcUpdateActionBuilder(requestName: Expression[String], tableName: Expression[String], values: Expression[String], where: Expression[String]) extends ActionBuilder {
 
   override def build(ctx: ScenarioContext, next: Action): Action = {
